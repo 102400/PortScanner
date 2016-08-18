4bc0807c8e51a4914927b34388e5f76e233fbf1f
dailiha01sy <dailiha01sy@gmail.com>
2016-08-18 22:42:27 +0800

    20160818a

---
+++ src/d/H.java
@@ -0,0 +1,89 @@
+package d;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class H implements Runnable {
+	
+	private String host;
+	private int port = 80;
+	private int timeOut = 3000;
+	
+	public H(String host)
+	{
+		this.host = host;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true)
+		{
+			if(scan(host, port, timeOut))
+			{
+	            System.out.println(host + ":" + 81);
+	        }
+			else
+			{
+//	        	System.out.println("X "+host);
+	        }
+			host = addIP(host);
+		}
+	}
+	
+	public boolean scan(String host, int port, int timeOut) {
+        boolean flag = false;
+        Socket socket = null;
+        try {
+            socket = new Socket(host,port);
+            socket.setSoTimeout(timeOut);
+            flag = true;
+        } catch (IOException e) {
+            // e.printStackTrace();
+        } finally{
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+        return flag;
+    }
+	
+	public String addIP(String host)
+    {
+    	String[] temp = host.split("\\.");
+		int temp_c = Integer.valueOf(temp[2]);
+		temp_c++;
+		temp[2] = String.valueOf(temp_c);
+		if(temp_c==256)
+		{
+			try
+			{
+				throw new Exception("Out of range");
+			}
+			catch(Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+    	String r = "";
+    	for(int i=0;i<temp.length;i++)
+    	{
+    		if(i!=3)
+    		{
+    			r = r + temp[i] + ".";
+    		}
+    		else
+    		{
+    			r = r + temp[i];
+    		}
+    	}
+    	
+    	return r;
+    }
+
+}
---
+++ src/d/PortScanner.java
@@ -0,0 +1,30 @@
+package d;
+ 
+public class PortScanner {
+	
+    public static void main(String[] args) {
+//    	String start_host = "40.4.0";
+    	String start_host = "123.202.0.";
+//    	String end_host;
+    	
+    	/**
+    	 * example...
+    	 * 
+    	 * thread  0           1           2           ...  255
+    	 * 0       40.4.0.0    40.4.1.0    40.4.2.0    ...  40.4.255.0
+    	 * 1       40.4.0.1    40.4.1.1    40.4.2.1    ...  40.4.255.1
+    	 * 2       40.4.0.2    40.4.1.2    40.4.2.2    ...  40.4.255.2
+    	 * ...     ...         ...         ...         ...  ...
+    	 * 255     40.4.0.255  40.4.1.255  40.4.2.255  ...  40.4.255.255
+    	 * 
+    	 */
+    	
+        for(int i=0;i<256;i++)  //Open 256 threads
+        {
+        	String host = start_host + i;
+        	H h = new H(host);
+        	Thread t = new Thread(h);
+        	t.start();
+        }
+    }
+}
